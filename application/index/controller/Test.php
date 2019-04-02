a20eb423c2b2eea2e82a9b79754c7fcb28c87bdd
legend <zhangtianshuo@emiaoqian.com>
2019-04-02 11:12:52 +0800

    1111

---
+++ application/index/controller/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: legend
+ * Date: 2019/4/2
+ * Time: 11:10
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Test extends Controller
+{
+    public function index()
+    {
+        dump(111);
+    }
+}
